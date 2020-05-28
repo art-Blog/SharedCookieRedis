f19c227ecf127dce4a7430c1ea10affe58f43a7e
art <art.huang@ehsn.com.tw>
2020-05-28 18:02:12 +0800

    int

---
+++ shared-cookie-redis/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace shared_cookie_redis.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
